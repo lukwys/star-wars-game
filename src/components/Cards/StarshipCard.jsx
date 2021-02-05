028bc9f3ef51df9bebe1c0f42aa2b30954b7b834
lwyso <lukasz-wysocki18@wp.pl>
2021-02-05 21:36:22 +0100

    create starship component

--- src/components/Cards/StarshipCard.jsx
+++ src/components/Cards/StarshipCard.jsx
@@ -1,5 +1,31 @@
-export const StarshipCard = () => {
+import { CardContent, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  heroDetails: {
+    listStyle: 'none',
+    padding: 0,
+    textAlign: 'left',
+    fontSize: 20,
+  },
+  heroData: {
+    minWidth: 250,
+    margin: '10px 0',
+    textTransform: 'capitalize',
+  }
+})
+
+export const StarshipCard = (props) => {
+  const classes = useStyles();
+  const { name, crew, hyperdrive_rating, passengers } = props.cardDetail;
+
   return (
-    <p>Starship Card</p>
+    <CardContent>
+      <ul className={classes.heroDetails}>
+        <li className={classes.heroData}>name: {name}</li>
+        <li className={classes.heroData}>crew: {crew}</li>
+        <li className={classes.heroData}>hyperdrive rating: {hyperdrive_rating}</li>
+        <li className={classes.heroData}>passengers: {passengers}</li>
+      </ul>
+    </CardContent>
   )
 }
